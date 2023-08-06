82efbfd89bfd0427aae901e30849f09a5c517ab3
Mathieu Dubart <mathieudubart@mini-de-mathieu.home>
2023-08-06 11:53:44 +0200

    :sparkles: initial commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Mathieu Dubart on 06/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
